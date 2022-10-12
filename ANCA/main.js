e6f36049b4a7168b4bd87561203af909bb2452cf
Mohammad Qataadah Zakir <80727404+MQZakir@users.noreply.github.com>
2022-10-12 18:18:41 +0300

    Add files via upload

---
+++ ANCA/main.js
@@ -0,0 +1,209 @@
+function home()
+{
+    window.location.assign("index.html");
+}
+
+function brand()
+{
+    window.location.assign("brands.html");
+}
+
+function qna()
+{
+    window.location.assign("qna.html");
+}
+
+function alfaromeo()
+{
+    window.location.assign("https://www.alfaromeo.com/");
+}
+
+function astonmartin()
+{
+    window.location.assign("https://www.astonmartin.com/en/");
+}
+
+function audi()
+{
+    window.location.assign("https://www.audi.com/en.html");
+}
+
+function bentley()
+{
+    window.location.assign("https://www.bentleymotors.com/en.html");
+}
+
+function bmw()
+{
+    window.location.assign("https://www.bmw.com/en/index.html");
+}
+
+function bugatti()
+{
+    window.location.assign("https://www.bugatti.com/");
+}
+
+function cadillac()
+{
+    window.location.assign("https://www.cadillac.com/");
+}
+
+function chevrolet()
+{
+    window.location.assign("https://www.chevrolet.com/");
+}
+
+function dodge()
+{
+    window.location.assign("https://www.dodge.com/");
+}
+
+function ferrari()
+{
+    window.location.assign("https://www.ferrari.com/");
+}
+
+function ford()
+{
+    window.location.assign("https://www.ford.com/");
+}
+
+function genesis()
+{
+    window.location.assign("https://www.genesis.com/worldwide/en/main.html");
+}
+
+function gmc()
+{
+    window.location.assign("https://www.gmc.com/gmc-worldwide");
+}
+
+function honda()
+{
+    window.location.assign("https://global.honda/");
+}
+
+function hyundai()
+{
+    window.location.assign("https://www.hyundai.com/worldwide/en/");
+}
+
+function jaguar()
+{
+    window.location.assign("https://www.jaguar.com/index.html");
+}
+
+function jeep()
+{
+    window.location.assign("https://www.jeep.com/");
+}
+
+function kia()
+{
+    window.location.assign("https://worldwide.kia.com/eu/sustainability");
+}
+
+function koe()
+{
+    window.location.assign("https://www.koenigsegg.com/");
+}
+
+function lambo()
+{
+    window.location.assign("https://www.lamborghini.com/en-en");
+}
+
+function landrover()
+{
+    window.location.assign("https://www.landrover.com/index.html");
+}
+
+function lotus()
+{
+    window.location.assign("https://www.lotuscars.com/");
+}
+
+function maserati()
+{
+    window.location.assign("https://www.maserati.com/global/en");
+}
+
+function mazda()
+{
+    window.location.assign("https://www.mazda.com/");
+}
+
+function mclaren()
+{
+    window.location.assign("https://www.mclaren.com/");
+}
+
+function merc()
+{
+    window.location.assign("https://www.mercedes-benz.com/en/");
+}
+
+function mini()
+{
+    window.location.assign("https://www.mini.com/home.html");
+}
+
+function mits()
+{
+    window.location.assign("https://www.mitsubishicars.com/");
+}
+
+function nissan()
+{
+    window.location.assign("https://www.nissan-global.com/EN/");
+}
+
+function pagani()
+{
+    window.location.assign("https://www.pagani.com/");
+}
+
+function peugeot()
+{
+    window.location.assign("https://www.peugeot.com/en/");
+}
+
+function porsche()
+{
+    window.location.assign("https://www.porsche.com/");
+}
+
+function rimac()
+{
+    window.location.assign("https://www.rimac-automobili.com/");
+}
+
+function rivian()
+{
+    window.location.assign("https://rivian.com/");
+}
+
+function rolls()
+{
+    window.location.assign("https://www.rolls-roycemotorcars.com/");
+}
+
+function tesla()
+{
+    window.location.assign("https://www.tesla.com/");
+}
+
+function toy()
+{
+    window.location.assign("https://global.toyota/en/");
+}
+
+function volks()
+{
+    window.location.assign("https://www.vw.com/en.html");
+}
+
+function volvo()
+{
+    window.location.assign("https://www.volvo.com/en/");
+}
